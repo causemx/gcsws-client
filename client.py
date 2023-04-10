79ef5492b16328f37e1ded2a353f7a1d1b5ae71f
Nanji Owl <allen.cause@gmail.com>
2023-04-10 14:20:01 +0800

    Add data to extension_dummy for position changed.

--- client.py
+++ client.py
@@ -4,13 +4,15 @@ import extensions
 
 async def serve_client(uri, data, time_span):
     async with websockets.connect(uri) as websocket:
-        for _ in range(10):
-            # await websocket.send(",".join(str(pt) for pt in extensions.PTLATLNG))
-            # print(f"(client) send to server: {dummy.PTLATLNG}")
-            await websocket.send(data)
-            _recv = await websocket.recv()
-            print(f"(client) recv from server {_recv}")
-            await asyncio.sleep(time_span)
+        # await websocket.send(",".join(str(pt) for pt in extensions.PTLATLNG))
+        # print(f"(client) send to server: {dummy.PTLATLNG}")
+        while True:
+            for d in data:
+                await websocket.send(d)
+                _recv = await websocket.recv()
+                print(f"(client) recv from server {_recv}")
+                await asyncio.sleep(time_span)
+        
 
 loop = asyncio.new_event_loop()
 asyncio.set_event_loop(loop)
